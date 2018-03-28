333fc694b2e8c850032b50b5406f8a12fd142894
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-03-28 12:59:08 -0600

    Make tabs content show on click of tab

---
+++ public/lib/script.js
@@ -0,0 +1,18 @@
+const handleClick = event => {
+  const activeTab = event.target;
+  const tabNum = activeTab.innerText.slice(-1);
+  const allTabs = $('.tab')
+  const tabsArray = [...allTabs];
+  const inactiveTabs = tabsArray.filter(h3 => h3 != activeTab)
+  const allContentDivs = $('.tab-content')
+  const divsArray = [...allContentDivs];
+  const activeDiv = divsArray.find(div => div.id === `tab-${tabNum}`)
+  const inactiveDivs = divsArray.filter(div => div != activeDiv)
+
+  activeTab.setAttribute('class', 'tab button-active')
+  activeDiv.setAttribute('class', 'tab-content active');
+  inactiveTabs.forEach(tab => tab.setAttribute('class', 'tab button-inactive'))
+  inactiveDivs.forEach(div => div.setAttribute('class', 'tab-content inactive'))
+}
+
+$('.tab').on('click', handleClick)
